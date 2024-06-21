0400904adb1061eb98b672e627798f25b6125753
Joshua Dubey <jodubey@deloitte.co.uk>
2024-06-21 17:08:15 +0100

    fix:  silence compiler warnings

--- Tests/CoordinationTests/TabCoordinatorTests.swift
+++ Tests/CoordinationTests/TabCoordinatorTests.swift
@@ -1,10 +1,11 @@
 @testable import Coordination
 import XCTest
 
-@MainActor
+
 final class TabCoordinatorTests: XCTestCase {
     var sut: MockTabCoordinator!
     
+    @MainActor
     override func setUp() {
         sut = MockTabCoordinator()
     }
@@ -15,11 +16,13 @@ final class TabCoordinatorTests: XCTestCase {
 }
 
 extension TabCoordinatorTests {
+    @MainActor
     func testCoordinatorStart() {
         sut.start()
         XCTAssertTrue(sut.coordinatorDidStart)
     }
     
+    @MainActor
     func testAddingTab() {
         XCTAssertEqual(sut.childCoordinators.count, 0)
         XCTAssertNil(sut.root.tabBar.items)
