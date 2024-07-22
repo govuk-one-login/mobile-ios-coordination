ed2460883fa054fe691f7da667ba4a2ae7a98ac6
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-07-22 15:45:36 +0100

    fix: making class

--- Sources/Coordination/Flows/TabCoordinator.swift
+++ Sources/Coordination/Flows/TabCoordinator.swift
@@ -4,9 +4,47 @@ import UIKit
 ///
 /// A coordinator which displays one or more view controllers within a UITabBarController context.
 @MainActor
-public protocol TabCoordinator: Coordinator, UITabBarControllerDelegate {
+public protocol TabCoordinator: Coordinator {
     var root: UITabBarController { get }
 }
+
+class TabSomething: NSObject, TabCoordinator {
+    private (set) var root: UITabBarController
+    var childCoordinators: [any ChildCoordinator] = []
+    
+    init(root: UITabBarController) {
+        self.root = root
+    }
+    
+    public func start() {
+        // Empty Implementation
+    }
+}
+
+extension TabSomething: UITabBarControllerDelegate, ParentCoordinator {
+    func tabBarController(_ tabBarController: UITabBarController,
+                          didSelect viewController: UIViewController) {
+
+        guard let selectedChild = (childCoordinators as? [any AnyCoordinator])?
+            .first(where: { $0.root == viewController }) as? TabItemCoordinator else { return }
+        selectedChild.didBecomeSelected()
+    }
+}
+
+//public extension TabCoordinator where Self: ParentCoordinator {
+//    func tabBarController(_ tabBarController: UITabBarController,
+//                          didSelect viewController: UIViewController) {
+//
+//        guard let selectedChild = (childCoordinators as? [any AnyCoordinator])?
+//            .first(where: { $0.root == viewController }) as? TabItemCoordinator else { return }
+//        selectedChild.didBecomeSelected()
+//    }
+//}
+
+public protocol TabItemCoordinator: Coordinator {
+    func didBecomeSelected()
+}
+
 
 public extension TabCoordinator where Self: ParentCoordinator {
     /// Opens a child coordinator flow within the root tab bar controller
@@ -39,18 +77,3 @@ public extension TabCoordinator where Self: ParentCoordinator {
 //        }
 //    }
 //}
-
-public extension TabCoordinator where Self: ParentCoordinator {
-    @MainActor
-    func tabBarController(_ tabBarController: UITabBarController,
-                          didSelect viewController: UIViewController) {
-
-        guard let selectedChild = (childCoordinators as? [any AnyCoordinator])?
-            .first(where: { $0.root == viewController }) as? TabItemCoordinator else { return }
-        selectedChild.didBecomeSelected()
-    }
-}
-
-public protocol TabItemCoordinator: Coordinator {
-    func didBecomeSelected()
-}
