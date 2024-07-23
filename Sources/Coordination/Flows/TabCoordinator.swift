c4518713e6b377b55e559073b10bb87668b9a44f
Oliver BINNS <oliver.binns@digital.cabinet-office.gov.uk>
2024-07-23 19:05:56 +0100

    refactor: revert requirement for TabCoordinator to be a class

--- Sources/Coordination/Flows/TabCoordinator.swift
+++ Sources/Coordination/Flows/TabCoordinator.swift
@@ -3,6 +3,7 @@ import UIKit
 /// Tab Coordinator
 ///
 /// A coordinator which displays one or more view controllers within a UITabBarController context.
+@available(*, deprecated, renamed: "TabCoordinatorV2", message: "This protocol is deprecated and will be removed in a future release")
 @MainActor
 public protocol TabCoordinator: Coordinator {
     var root: UITabBarController { get }
@@ -18,37 +19,4 @@ public extension TabCoordinator where Self: ParentCoordinator {
         root.addChild(childCoordinator.root)
         openChild(childCoordinator)
     }
-}
-
-
-open class TabNavigationCoordinator : NSObject, TabCoordinator {
-    private (set) public var root: UITabBarController
-    public var childCoordinators: [any ChildCoordinator] = []
-    
-    public init(root: UITabBarController) {
-        self.root = root
-    }
-    
-    open func start() {
-        // Empty Implementation
-    }
-    
-    open func didRegainFocus(fromChild child: (any ChildCoordinator)?) {
-        // Empty Implementation
-    }
-}
-
-extension TabNavigationCoordinator : UITabBarControllerDelegate, ParentCoordinator {
-    public func tabBarController(_ tabBarController: UITabBarController,
-                          didSelect viewController: UIViewController) {
-
-        guard let selectedChild = (childCoordinators as? [any AnyCoordinator])?
-            .first(where: { $0.root == viewController }) as? TabItemCoordinator else { return }
-        selectedChild.didBecomeSelected()
-    }
-}
-
-@MainActor
-public protocol TabItemCoordinator: Coordinator {
-    func didBecomeSelected()
 }
