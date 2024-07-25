334c977aa1d59d9bf5c9165db2880492c6073fb0
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-07-25 10:26:32 +0100

    tests: adding tests

---
+++ Tests/CoordinationTests/V2TabCoordinatorTests.swift
@@ -0,0 +1,43 @@
+@testable import Coordination
+import XCTest
+
+
+final class V2TabCoordinatorTests: XCTestCase {
+    var sut: MockTabCoordinatorV2!
+    
+    @MainActor
+    override func setUp() {
+        sut = MockTabCoordinatorV2()
+    }
+    
+    override func tearDown() {
+        sut = nil
+    }
+}
+
+extension V2TabCoordinatorTests {
+    @MainActor
+    func testCoordinatorStart() {
+        sut.start()
+        XCTAssertTrue(sut.coordinatorDidStart)
+    }
+    
+    @MainActor
+    func testAddingTab() throws {
+        XCTAssertEqual(sut.childCoordinators.count, 0)
+        XCTAssertNil(sut.root.tabBar.items)
+        
+        sut.start()
+        sut.addTabs()
+        
+        guard let vc = sut.root.viewControllers?[0] else {
+            XCTFail("no vc's set")
+            return
+        }
+        
+        sut.root.delegate?.tabBarController?(sut.root, didSelect: vc)
+        
+        let child = try XCTUnwrap(sut.childCoordinators.first as? MockChildTabV2Coordinator)
+        XCTAssertTrue(child.did_becomeSelected)
+    }
+}
