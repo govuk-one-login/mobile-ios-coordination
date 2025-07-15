f3c8e130e0718b24c2faa4ff6a3073f41264e5cd
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2025-07-15 14:12:23 +0100

    feat: added willRegainFocus

--- Sources/Coordination/ParentCoordinator.swift
+++ Sources/Coordination/ParentCoordinator.swift
@@ -9,6 +9,7 @@ public protocol ParentCoordinator: Coordinator {
     var childCoordinators: [ChildCoordinator] { get set }
 
     func didRegainFocus(fromChild child: ChildCoordinator?)
+    func willRegainFocus(fromChild child: ChildCoordinator?)
     func performChildCleanup(child: ChildCoordinator)
 }
 
@@ -32,12 +33,18 @@ public extension ParentCoordinator {
     /// Child Did Finish.
     /// Call this method from concrete implementations of `ChildCoordinator` within the `end` method.
     func childDidFinish(_ child: ChildCoordinator) {
+        willRegainFocus(fromChild: child)
         for (index, coordinator) in childCoordinators.enumerated() where coordinator === child {
             childCoordinators.remove(at: index)
             break
         }
         performChildCleanup(child: child)
         didRegainFocus(fromChild: child)
+    }
+    
+    func willRegainFocus(fromChild child: ChildCoordinator?) {
+        // Default implementation is empty.
+        // Override this is if response if required before a child finishes.
     }
     
     func didRegainFocus(fromChild child: ChildCoordinator?) {
